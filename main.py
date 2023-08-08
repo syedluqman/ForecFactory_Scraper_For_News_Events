62d78f89e54ecfda726c2c084d6ba04f373be92b
Syed Luqman <32990257+syedluqman@users.noreply.github.com>
2023-08-08 23:10:49 +0100

    Update main.py
    With date changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 import re
 import os
+import csv
 
 def get_date_time(event_time):
     match = re.search(r'(\d+)(:\d{2})([ap]m)', event_time)
@@ -12,19 +13,23 @@ def get_date_time(event_time):
         return f"{hour:02}:{minutes}"
     return None
 
-def scrape_forex_factory_last_month(file_path):
+def scrape_forex_factory_between_dates(start_date, end_date, file_path):
     scraper = cloudscraper.create_scraper()
     
-    # Calculate start and end dates for the last month
-    today = datetime.today()
-    last_month_start = (today.replace(day=1) - timedelta(days=1)).replace(day=1)
-    last_month_end = today.replace(day=1) - timedelta(days=1)
+    current_date = start_date
     
-    current_date = last_month_start.strftime("%b%d.%Y")
-    end_date = last_month_end.strftime("%b%d.%Y")
+    # Create column names
+    columns = ['Date', 'Time', 'Currency', 'Impact', 'Event', 'Previous', 'Forecast', 'Actual']
+    
+    # Check if the CSV file exists, and generate if not
+    if not os.path.exists(file_path):
+        with open(file_path, 'w', newline='') as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(columns)
     
     while current_date <= end_date:
-        url = f'https://www.forexfactory.com/calendar?day={current_date}'
+        url_date_format = current_date.strftime("%b%d.%Y")  # Format for URL
+        url = f'https://www.forexfactory.com/calendar?day={url_date_format}'
         page = scraper.get(url).text
         soup = BeautifulSoup(page, 'html.parser')
         table = soup.find('table', class_='calendar__table')
@@ -42,15 +47,21 @@ def scrape_forex_factory_last_month(file_path):
                     forecast = row.find('td', class_='forecast').text.strip()
                     actual = row.find('td', class_='actual').text.strip()
                     
-                    with open(file_path, 'a') as file:
+                    with open(file_path, 'a', newline='') as file:
+                        csv_writer = csv.writer(file)
                         # Write the extracted data to the file
-                        file.write(f"{current_date}, {event_time}, {currency}, {impact}, {event}, {previous}, {forecast}, {actual}\n")
+                        csv_writer.writerow([url_date_format, event_time, currency, impact, event, previous, forecast, actual])
         
         # Move to the next date
-        current_date = (datetime.strptime(current_date, "%b%d.%Y") + timedelta(days=1)).strftime("%b%d.%Y")
+        current_date += timedelta(days=1)
 
 if __name__ == "__main__":
-    output_file_path = "ffc_news_events_last_month.csv"
+    output_file_path = "ffc_news_events.csv"
     
-    # Call the scraping function for the last month
-    scrape_forex_factory_last_month(output_file_path)
+    start_date_input = input("Enter the start date (Format: MMMDD.YYYY): ")
+    end_date_input = input("Enter the end date (Format: MMMDD.YYYY): ")
+    start_date = datetime.strptime(start_date_input, "%b%d.%Y")
+    end_date = datetime.strptime(end_date_input, "%b%d.%Y")
+    
+    # Call the scraping function between the specified dates
+    scrape_forex_factory_between_dates(start_date, end_date, output_file_path)
